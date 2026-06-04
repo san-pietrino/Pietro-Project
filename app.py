aa32e23182e5b7ff8a7f282ca7fa8e7fba2349b9
Alessia Piscioli <pisciolialessia@gmail.com>
2026-06-04 17:31:38 +0200

    127 to 000

--- app.py
+++ app.py
@@ -812,4 +812,5 @@ def notifications():
 # Initialize database on startup
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8001)
+       
